f6724909b8da1150eecf36579a09e71de0324b41
Ashley Towns <mail@ashleytowns.id.au>
2016-01-11 20:52:27 +1100

    hmm

--- Package.swift
+++ Package.swift
@@ -1,8 +1,3 @@
 import PackageDescription
 
-let package = Package(
-  name: "CGLFW",
-  dependencies: [
-    .Package(url: "https://github.com/aktowns/CGL.git", Version(1,0,0))
-  ]
-)
+let package = Package(name: "CGLFW")
